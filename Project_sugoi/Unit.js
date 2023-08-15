fe22af44e483d638bd9129c9cfbc05941c0c2163
Zujin8010 <134988455+Zujin8010@users.noreply.github.com>
2023-08-15 09:00:46 +0800

    update

--- Project_sugoi/Unit.js
+++ Project_sugoi/Unit.js
@@ -1,7 +1,7 @@
+const topNav = nav.offsetTop
 const moreButton = document.querySelector('.moreButton')
 moreButton.addEventListener('click', function(){
     document.querySelector('.moreText').classList.toggle('show')
     document.querySelector('.moreButton').classList.toggle('show')
 })
 
-const topNav = nav.offsetTop
